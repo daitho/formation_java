d21459e8d80b6dc2c7533faa49a6ef6c40603bec
gaston_adkins <gatotoum@ITEM-S146679.emea.msad.sopra>
2023-03-27 17:26:14 +0200

    Classe Client

---
+++ src/ecommerce/Client.java
@@ -0,0 +1,13 @@
+package ecommerce;
+
+public class Client {
+	private String idClient;
+	private String nom;
+	private String prenom;
+	private String code_postale;
+	private String ville;
+	private String pays;
+	private String region;
+	
+
+}
